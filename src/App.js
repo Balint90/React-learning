f055bbe28e204645c8d3d80baca0e0d271a445bb
Bálint Csertán <Balint90@users.noreply.github.com>
2022-07-11 23:56:41 +0200

    Conditionals

--- src/App.js
+++ src/App.js
@@ -5,23 +5,32 @@ const body = 'This is my blogpost'
 const comments = [
     {id: 1, text: 'Comment one'},
     {id: 2, text: 'Comment two'},
-    {id: 3, text: 'Comment three'},
+    {id: 3, text: 'Comment three'}
 ]
+
+const loading = false
+const showComments = false
+
+const commentBlock = (<div className="comments">
+<h3>Comments ({comments.length})</h3>
+<ul>
+    {comments.map((comment, index) => (
+        <li key={index}>{comment.text}</li>
+    ))}
+</ul>
+</div>)
 
 function App() {
     // return React.createElement("div", {className: 'container'}), React.createElement('h1', {}, 'My App')
+    if (loading) return <h1>Loading...</h1>
     return (
     <div className="container">
         <h1>{title.toUpperCase()}</h1>
         <p>{body}</p>
-        <div className="comments">
-            <h3>Comments ({comments.length})</h3>
-            <ul>
-                {comments.map((comment, index) => (
-                    <li key={index}>{comment.text}</li>
-                ))}
-            </ul>
-        </div>
+
+        {showComments && commentBlock}
+
+        
     </div>)
 }
 
