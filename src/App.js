d15e104c8acc21cec0d86e252f9e83c91a7e9beb
Bálint Csertán <Balint90@users.noreply.github.com>
2022-07-11 09:38:22 +0200

    Dynamic Values and lists

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const title = "Blog post"
+const body = 'This is my blogpost'
+const comments = [
+    {id: 1, text: 'Comment one'},
+    {id: 2, text: 'Comment two'},
+    {id: 3, text: 'Comment three'},
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // return React.createElement("div", {className: 'container'}), React.createElement('h1', {}, 'My App')
+    return (
+    <div className="container">
+        <h1>{title.toUpperCase()}</h1>
+        <p>{body}</p>
+        <div className="comments">
+            <h3>Comments ({comments.length})</h3>
+            <ul>
+                {comments.map((comment, index) => (
+                    <li key={index}>{comment.text}</li>
+                ))}
+            </ul>
+        </div>
+    </div>)
 }
 
-export default App;
+export default App
