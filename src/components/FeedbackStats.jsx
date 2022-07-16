7d4844711f15e0131d240d7dc32a08e39ab266f6
Bálint Csertán <Balint90@users.noreply.github.com>
2022-07-16 23:59:03 +0200

    Add FeedbackForm

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 
 function FeedbackStats({feedback}) {
     let average = feedback.reduce((acc, cur) => {
-        return acc+ cur.rating;
+        return acc + cur.rating;
     }, 0) /feedback.length;
     
     average = average.toFixed(1).replace(/[.,]0$/, '');
